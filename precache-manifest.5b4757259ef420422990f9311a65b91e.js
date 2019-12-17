f0386cd9b3a1b376f951302d74fead197936ee03
unknown <codeprof2016@hotmail.com>
2019-12-18 00:42:29 +0200

    Updates

---
+++ precache-manifest.5b4757259ef420422990f9311a65b91e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f104934405b2ef069eda1f4c53386ed",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3b8ace4c756d999e3540",
+    "url": "/monsters-rolodex/static/css/main.ad8052c6.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b8ace4c756d999e3540",
+    "url": "/monsters-rolodex/static/js/main.1cc48f73.chunk.js"
+  },
+  {
+    "revision": "dc25ba12107eb546721c",
+    "url": "/monsters-rolodex/static/js/runtime-main.75c8e44f.js"
+  }
+]);
